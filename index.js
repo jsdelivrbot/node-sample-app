2e7196b7b0877e2c4a4dc0648dd47ce4eff19ed4
Balkrishna Pandey <sachit.nep@gmail.com>
2017-08-10 04:17:31 +0000

    Purple

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('<body bgcolor="red">Hello World!</body>')
+  response.send('<body bgcolor="purple">Hello World!</body>')
 })
 
 app.listen(app.get('port'), function() {
